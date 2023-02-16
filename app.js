d9024b7f71b10222e5a484e79cc6f16bbb49adb8
Leandro Strifezza <leandro.strifezza@xyclon.com.ar>
2023-02-16 13:52:09 -0300

    commit

--- app.js
+++ app.js
@@ -19,3 +19,24 @@ function closeModal(){
     modal.classList.remove("showModal")
 }
 
+
+
+//Side Bar 
+function openNav() {
+    document.getElementById("mySidebar").style.width = "50%";
+    document.getElementById("openBtn").style.marginLeft = "0";
+    document.getElementById("openBtn").style.opacity= "0";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("openBtn").style.marginLeft= "0";
+    document.getElementById("openBtn").style.opacity= "1";
+  }
+
+
+  //Iframe
+  function openVideo(){
+    document.getElementById("video").style.width="90vw"
+    document.getElementById("video").style.height="90vw"
+  }
