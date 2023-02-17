a85fce802d950de6bb1629c7a5412222e2da1534
Leandro Strifezza <leandro.strifezza@xyclon.com.ar>
2023-02-17 18:28:37 -0300

    ajuste animacion

--- app.js
+++ app.js
@@ -1,59 +1,60 @@
 
-  //Intro de la Web
-   setTimeout(function() {
-     document.getElementById("intro-section").style.opacity = "0";
-     document.getElementById("intro-section").style.zIndex="-2"
- }, 5000);
+
 
 
 const modal = document.getElementById("modal")
 const cardBtn = document.getElementsByClassName("chart-cardflip")
 const backBtn = document.getElementById("backBtn")
 
-for (let i = 0; i < cardBtn.length; i++) {
-    cardBtn[i].addEventListener('click', showModal);
-  }
+//Intro de la Web, para que solo se ejecute la primera vez que se entra
+const introSection = document.getElementById("intro-section")
+if (sessionStorage.getItem("animacion-intro")) {
+    introSection.style.animation ="none"
+         introSection.style.opacity = "0";
+         introSection.style.zIndex = "-2"
+     } else {
+        sessionStorage.setItem("animacion-intro", "true")
+     }
+  
 
-backBtn.addEventListener("click", closeModal)
 
-
-
-
-function showModal(){
-    modal.classList.add("showModal")
+function showModal() {
+  modal.classList.add("showModal")
 }
-function closeModal(){
-    modal.classList.remove("showModal")
+function closeModal() {
+  modal.classList.remove("showModal")
 }
+
+
 
 
 
 //Side Bar 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "50%";
-    document.getElementById("openBtn").style.marginLeft = "0";
-    document.getElementById("openBtn").style.opacity= "0";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("openBtn").style.marginLeft= "0";
-    document.getElementById("openBtn").style.opacity= "1";
-  }
+  document.getElementById("mySidebar").style.width = "50%";
+  document.getElementById("openBtn").style.marginLeft = "0";
+  document.getElementById("openBtn").style.opacity = "0";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("openBtn").style.marginLeft = "0";
+  document.getElementById("openBtn").style.opacity = "1";
+}
 
 
-  //Iframe
-  function openVideo(){
-    document.getElementById("video").style.width="90vw"
-    document.getElementById("video").style.height="90vw"
-  }
+//Iframe
+function openVideo() {
+  document.getElementById("video").style.width = "90vw"
+  document.getElementById("video").style.height = "90vw"
+}
 
-  //Funcion para Links del home
+//Funcion para Links del home
 
-  function cerveza(){
-    window.location.href="./nuestras-cervezas.html"
-  }
-  
+function cerveza() {
+  window.location.href = "./nuestras-cervezas.html"
+}
+
 
 
 
